f5d8b7d49a9783b485d55479e75a31c88ecd8046
Chandrakant Devani <chandrakant@silverwebbuzz.com>
2019-10-04 12:20:01 +0530

    New Project

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ModalpagePage } from '../modalpage/modalpage.page';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,26 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private modalCtrl: ModalController) {}
+
+  async OpenModel(){
+    const presentModel = await this.modalCtrl.create({
+      component: ModalpagePage,
+      componentProps: {
+        title: 'Billing Address',
+        type:'billing',
+      },
+      showBackdrop: true,
+      mode:	"ios",
+      cssClass: 'change-address-shipping-modal'
+    });
+
+    presentModel.onWillDismiss().then((data)=>{
+      console.log(data);
+      //custom code
+    });
+    
+    return await presentModel.present();
+  }
 
 }
